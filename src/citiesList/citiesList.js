b3bd1a07c7fbe92fe26449ccf65080b97927bd40
rube4489 <ruben.lopez@doc24.com.ar>
2022-08-24 00:59:38 -0300

    Agrego lista de ciudades

---
+++ src/citiesList/citiesList.js
@@ -0,0 +1,7 @@
+export const citiesList = [
+  { name: "Buenos Aires", id: 1, lat: "-34.61315", long: "-58.37723" },
+  { name: "Cordoba", id: 2, lat: "-31.4135", long: "-64.18105" },
+  { name: "Ushuaia", id: 3, lat: "-54.81084", long: "-68.31591" },
+  { name: "Jujuy", id: 4, lat: "-24.1945", long: "-65.29712" },
+  { name: "Mendoza", id: 5, lat: "-32.89084", long: "-68.82717" },
+];
